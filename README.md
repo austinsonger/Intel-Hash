6f54c75d98f8d399bd474e9d2339d9135971842f
austinsonger <austinsonger@protonmail.com>
2021-01-05 16:05:57 -0600

    Update

---
+++ README.md
@@ -0,0 +1,14 @@
+> Not Ready for Production Yet
+
+
+# IntelHash
+Takes a Onion URL and Hashes it and compares it against blacklisted hashed onion URLS
+
+
+
+### To Do
+
+- [ ] Feature: Compare Hashed Onion URL with Ahmia [Blacklist Hashed Onion URLS](https://ahmia.fi/blacklist/banned/)
+- [ ] Feature: Returns Result - "Match Found" or "Match Not Found"
+- [ ] Feature: Hash Regular URL and Check Hash on VirusTotal
+- [ ] Feature: Hash File and Check Hash on VirusTotal
--- main.py
+++ main.py
@@ -1,16 +1,65 @@
-# This is a sample Python script.
+# !/usr/bin/env python3
+"""
+    Title:      IntelHash
+    Desc:       Takes a Onion URL and Hashes it and compares it against blacklisted hashed onion URLS
+    Author:     Austin Songer
+    Version:    0.0.1
+    GitHub URL: https://github.com/austinsonger/intelhash
+"""
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import base64
+import hashlib
+import html.parser
+import re
+import json
+import time
+import os
+import socket
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def switchMenu(choice):
+    if choice == '1':
+        hashMenu()
+    if choice == '0':
+        exit()
+    else:
+        mainMenu()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def hashSwitch(choice):
+    if choice == '1':
+        hashOnion()
+    if choice == '0':
+        mainMenu()
+
+
+def mainMenu():
+    print("\n --------------------------------- ")
+    print("\n              IntelHash            ")
+    print("\n --------------------------------- ")
+    print(" What would you like to do? ")
+    print("\n OPTION 1: Hashing ")
+    print(" OPTION 0: Exit Tool")
+    switchMenu(input())
+
+
+def hashMenu():
+    print("\n -------------------------------")
+    print("             Hashing              ")
+    print(" ---------------------------------")
+    print(" What would you like to do? ")
+    print(" OPTION 1: Enter Onion URL and Hash it")
+    print(" OPTION 0: Exit to Main Menu")
+    hashSwitch(input())
+
+
+def hashOnion():
+    userinput = input(" Enter the URL to be hashed: ")
+    print(" MD5 Hash: " + hashlib.md5(userinput.encode("utf-8")).hexdigest())
+    hashMenu()
+
+
+
+
